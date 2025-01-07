5ba6394dfd86c2510b0e272a05a6990accd77cd3
kk <mengkai.li@accenture.com>
2025-01-07 13:22:38 +0900

    Refactor auth challenge logic in define-auth-challenge handler
    Simplified and clarified the authentication logic for handling Cognito custom challenges. Introduced proper constants, refactored conditions for better readability, and added a helper function to validate session challenges. This improves maintainability and ensures consistent challenge validation.

--- functions/define-auth-challenge/main.go
+++ functions/define-auth-challenge/main.go
@@ -7,19 +7,43 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+const (
+	challengeNameCustomChallenge = "CUSTOM_CHALLENGE"
+)
+
 func handler(ctx context.Context, event events.CognitoEventUserPoolsDefineAuthChallenge) (events.CognitoEventUserPoolsDefineAuthChallenge, error) {
-	if len(event.Request.Session) == 0 {
-		event.Response.ChallengeName = "CUSTOM_CHALLENGE"
+	if len(event.Request.Session) > 0 &&
+		hasInvalidSession(event.Request.Session) {
+		// We only accept custom challenges; fail auth
 		event.Response.IssueTokens = false
-		event.Response.FailAuthentication = false
-	} else if event.Request.Session[len(event.Request.Session)-1].ChallengeResult {
+		event.Response.FailAuthentication = true
+	} else if len(event.Request.Session) > 3 &&
+		!event.Request.Session[len(event.Request.Session)-1].ChallengeResult {
+		// The user provided a wrong answer 3 times; fail auth
+		event.Response.IssueTokens = false
+		event.Response.FailAuthentication = true
+	} else if len(event.Request.Session) > 0 &&
+		event.Request.Session[len(event.Request.Session)-1].ChallengeName == challengeNameCustomChallenge &&
+		event.Request.Session[len(event.Request.Session)-1].ChallengeResult {
+		// The user provided the right answer; succeed auth
 		event.Response.IssueTokens = true
 		event.Response.FailAuthentication = false
 	} else {
+		// The user did not provide a correct answer yet; present challenge
+		event.Response.ChallengeName = challengeNameCustomChallenge
 		event.Response.IssueTokens = false
-		event.Response.FailAuthentication = true
+		event.Response.FailAuthentication = false
 	}
 	return event, nil
+}
+
+func hasInvalidSession(sessions []*events.CognitoEventUserPoolsChallengeResult) bool {
+	for _, s := range sessions {
+		if s.ChallengeName != challengeNameCustomChallenge {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
